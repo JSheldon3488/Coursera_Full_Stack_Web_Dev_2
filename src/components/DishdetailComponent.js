c5f4457445662e00909828b72a75d7cf0419a916
Justin Sheldon <JSheldon3488@Gmail.com>
2020-12-17 13:37:01 -0600

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -30,6 +30,7 @@ class CommentForm extends Component {
     handleSubmit = (values) => {
         console.log("Current State is: " + JSON.stringify(values));
         alert("Current State is: " + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         this.toggle();
     }
     
@@ -87,32 +88,6 @@ class CommentForm extends Component {
     }
 }
 
-const DishDetail = (props) => {
-    if (props.dish != null) {
-        return (
-            <div class="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish}/>
-                    <RenderComments comments={props.comments}/>
-                </div>
-            </div>
-        );
-    }
-    else {
-        return (<div></div>);
-    }
-}
-
 function RenderDish({dish}) {
     return(
         <div className="col-12 col-md-5 m-1">
@@ -127,7 +102,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
     if (comments != null) {
         const data = comments.map((comment) => {
             return (
@@ -145,11 +120,37 @@ function RenderComments({comments}) {
                 <ul className="list-unstyled">
                     {data}
                 </ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment}/>
             </div>
         );
     }
     else { 
+        return (<div></div>);
+    }
+}
+
+const DishDetail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div class="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish}/>
+                    <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id}/>
+                </div>
+            </div>
+        );
+    }
+    else {
         return (<div></div>);
     }
 }
